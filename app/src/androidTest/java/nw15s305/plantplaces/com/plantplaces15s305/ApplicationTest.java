654e7aff3b167162e6623fdaf85b5e9a93bf066b
jonesbr <brandan.jones@uc.edu>
2015-03-28 16:58:59 -0400

    Initial commit

---
+++ app/src/androidTest/java/nw15s305/plantplaces/com/plantplaces15s305/ApplicationTest.java
@@ -0,0 +1,13 @@
+package nw15s305.plantplaces.com.plantplaces15s305;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
