6543ffeb742b433a4d87e3ee595e9705d15ae38e
jonesbr <brandan.jones@uc.edu>
2015-04-08 21:02:04 -0400

    Invoke image gallery with an implicit intent

--- app/src/main/java/nw15s305/plantplaces/com/plantplaces15s305/ColorCaptureActivity.java
+++ app/src/main/java/nw15s305/plantplaces/com/plantplaces15s305/ColorCaptureActivity.java
@@ -1,12 +1,20 @@
 package nw15s305.plantplaces.com.plantplaces15s305;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.File;
 
 
 public class ColorCaptureActivity extends PlantPlacesActivity {
+
+    public static final int IMAGE_GALLERY_REQUEST = 20;
 
     @Override
     public int getCurrentMenuId() {
@@ -17,6 +25,27 @@ public class ColorCaptureActivity extends PlantPlacesActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_capture);
+    }
+
+    /**
+     * This method will be invoked when the user clicks a button
+     * @param v
+     */
+    public void onImageGalleryClicked(View v) {
+        // invoke the image gallery using an implict intent.
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+        // where do we want to find the data?
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+        // finally, get a URI representation
+        Uri data = Uri.parse(pictureDirectoryPath);
+
+        // set the data and type.  Get all image types.
+        photoPickerIntent.setDataAndType(data, "image/*");
+
+        // we will invoke this activity, and get something back from it.
+        startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
     }
 
 
